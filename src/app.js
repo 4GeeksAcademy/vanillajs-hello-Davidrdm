4375406ab844fe6bf1911c1a323390a1731d2681
Davidruiz03061976 <davidruizdemanuel39@gmail.com>
2025-03-06 18:16:11 +0000

    firts

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-import "bootstrap";
-import "./style.css";
+
+window.onload = function () {
+  const topSuit = document.querySelector(".top-suit");
+  const number = document.querySelector(".number");
+  const bottomSuit = document.querySelector(".bottom-suit");
+  console.log(topSuit)
+  console.log(number)
+  console.log(bottomSuit)
+  let palosPoquer = ['♦', '♥', '♠', '♣'];
+  let numerosCarta = ['K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2', 'A'];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let randomPalos = Math.floor(Math.random() * palosPoquer.length);
+  let randomCarta = Math.floor(Math.random() * numerosCarta.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  topSuit.innerHTML = palosPoquer[randomPalos];
+  bottomSuit.innerHTML = palosPoquer[randomPalos];
+  number.innerHTML = numerosCarta[randomCarta];
+
+  if (palosPoquer[randomPalos] === '♦' || palosPoquer[randomPalos] === '♥') {
+    topSuit.classList.add('red')
+    bottomSuit.classList.add('red')
+  }
+
+
+}
